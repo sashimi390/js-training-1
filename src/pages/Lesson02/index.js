c303889a8f0a34f026a0049f954922bd62303266
sashimi390 <67630672+sashimi390@users.noreply.github.com>
2021-05-07 03:45:46 +0900

    lesson2

--- src/pages/Lesson02/index.js
+++ src/pages/Lesson02/index.js
@@ -4,7 +4,8 @@ import Chart from "../../components/Chart02";
 import instruction from "./instruction.md";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  input.sort((a, b) => a.count - b.count);
+  return input.slice(0, 20);
 };
 
 const Lesson = () => {
