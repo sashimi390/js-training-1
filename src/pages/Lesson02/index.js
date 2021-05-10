40636466b12a93b0fcee1b94800f540a4bb58d8b
sashimi390 <67630672+sashimi390@users.noreply.github.com>
2021-05-11 01:44:36 +0900

    ex2(re)

--- src/pages/Lesson02/index.js
+++ src/pages/Lesson02/index.js
@@ -4,7 +4,7 @@ import Chart from "../../components/Chart02";
 import instruction from "./instruction.md";
 
 const convertData = (input) => {
-  input.sort((a, b) => a.count - b.count);
+  input.sort((a, b) => b.count - a.count);
   return input.slice(0, 20);
 };
 
