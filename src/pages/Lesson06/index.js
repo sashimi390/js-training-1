f9c2a564e1751da2ec96ed5acefbb7b691707e58
sashimi390 <67630672+sashimi390@users.noreply.github.com>
2021-05-13 15:33:06 +0900

    lesson6

--- src/pages/Lesson06/index.js
+++ src/pages/Lesson06/index.js
@@ -4,7 +4,21 @@ import Chart from "../../components/Chart06";
 import instruction from "./instruction.md";
 
 const convertData = (input) => {
-  return []; // ここを作りましょう！
+  const data = [];
+  const colors = {
+    男性: "blue",
+    女性: "red"
+  }
+  for (let i = 0; i < input.length; i++) {
+    data.push({
+      color: colors[input[i].gender],
+      gender: input[i].gender,
+      bmi: input[i].x / (input[i].y / 100) ** 2,
+      weight: input[i].x,
+      height: input[i].y
+    });
+  }
+  return data;
 };
 
 const Lesson = () => {
